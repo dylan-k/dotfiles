9d74709370e4868cb828452dce7880720cda24e2
Dylan Kinnett <dkinnett@thewalters.org>
2023-05-11 17:06:20 -0400

    office dotfiles update

--- .config/powershell/Microsoft.PowerShell_profile.ps1
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,8 @@
 # DYLAN'S POWERSHELL PROFILE
 # like .bash_profile but for Windows PowerShell
 # store file in:
-# %USERPROFILE%\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
+# Windows - $Home\Documents\WindowsPowerShell\profile.ps1
+# Windows - $Home\Documents\WindowsPowerShell\Microsoft.VSCode_profile.ps1
 # Windows - $Home\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
 # Linux - ~/.config/powershell/Microsoft.Powershell_profile.ps1
 # macOS - ~/.config/powershell/Microsoft.Powershell_profile.ps1
@@ -10,17 +11,17 @@
 
 # PATH
 ################################################################################
-# $env:Path += ";$env:userprofile\AppData\Local\Programs"   
+# $env:Path += ";$env:userprofile\AppData\Local\Programs"
 
 
-# FUNCTIONS 
+# FUNCTIONS
 ################################################################################
 
 # update git repo and then open in vscode
 function workspace {
-  git fetch; 
+  git fetch;
   git status;
-  code . 
+  code .
 }
 
 # quickly edit this config file
@@ -31,7 +32,9 @@ function config {
 # hard reset git repository to latest online version
 function overwrite {
   git fetch;
-  git reset --hard origin/master
+  git reset --hard origin/main;
+  git reset --hard origin/master;
+
 }
 
 # delete empty directories recursively
@@ -47,13 +50,27 @@ function flatten {
   noempty
 }
 
+# open emacs in terminal by default
+# source: https://superuser.com/questions/339105/how-do-i-launch-emacs-in-terminal-mode-within-powershell
+# $emacs_wildcard = "C:\ProgramData\chocolatey\bin\emacs.exe"
+# if ($(Test-Path $emacs_wildcard)) {
+#   $emacs_path = (Get-ChildItem $emacs_wildcard)[-1].FullName -replace ' ', '` '
+#   function emacs { "$emacs_path -nw $args" | Invoke-Expression }
+# }
+
 # ALIASES
 ################################################################################
 
 function open { explorer.exe . }
 function home { set-location "H:\" }
+function documents { set-location "H:\Documents\" ; explorer.exe . }
+function projects { set-location "H:\Projects\" ; explorer.exe . }
+function docs { set-location "H:\Documents\" }
+function downloads { set-location "C:\Users\Dylan\Downloads\" }
 function sites { set-location "H:\Sites\" ; explorer.exe . }
+function notes { set-location "H:\Notes\" ; explorer.exe .; workspace }
 function valestyles { set-location "$env:userprofile\.config\vale"; workspace }
+function artsite { set-location "H:\Sites\art\" ; explorer.exe .; workspace }
 
 ## Moving around
 function root { cd / }
@@ -68,10 +85,10 @@ function up8 { cd ../../../../../../../.. }
 function up9 { cd ../../../../../../../../.. }
 function up10 { cd ../../../../../../../../../.. }
 
-New-Alias which get-command
+# New-Alias which get-command
 Remove-Item alias:\where -Force
 New-Alias where get-command -Force
- 
+
 
 # INIT
 ################################################################################
@@ -86,9 +103,10 @@ New-Alias where get-command -Force
 # Install with:
 #   Install-Module posh-git -Scope CurrentUser
 #   Install-Module oh-my-posh -Scope CurrentUser
-Import-Module posh-git
-Import-Module oh-my-posh
-Set-PoshPrompt -Theme powerline
+# Import-Module posh-git
+# $env:POSH_GIT_ENABLED = $true
+# Import-Module posh-git
+Invoke-Expression (&starship init powershell)
 
 # Chocolatey profile
 # $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
